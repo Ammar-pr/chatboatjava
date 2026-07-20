839227cf4f269fb53ce0accd278edb83cd9ada7b
ASUS <ASUS@LAPTOP-LP5CEJ16>
2026-07-20 19:39:42 +0300

    socket server side

---
+++ src/main/java/com/mycompany/javachatapp/Javachatapp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javachatapp;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Javachatapp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
