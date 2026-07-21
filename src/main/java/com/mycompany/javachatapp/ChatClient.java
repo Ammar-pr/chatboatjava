a5271b1ea2a0a3b4b2b059932984964ba4102fa2
ASUS <ASUS@LAPTOP-LP5CEJ16>
2026-07-21 19:57:53 +0300

    conf chat clint add socket print writer , buffer reader , to make the clints ready to connect to the server in the constructor of ClatClient class

--- src/main/java/com/mycompany/javachatapp/ChatClient.java
+++ src/main/java/com/mycompany/javachatapp/ChatClient.java
@@ -12,6 +12,25 @@ import java.net.*;
  */
 public class ChatClient {
    
+    private Socket socket=null;
+    private BufferedReader inputConsole=null;
+    private PrintWriter out = null;
+    private BufferedReader in= null;
+
+    public ChatClient(String address, int port) {
+    try {
+        socket= new Socket(address,port);
+        System.out.println("Connetecd to the chat server");
+        inputConsole = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(socket.getOutputStream(),true);
+        in= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        
+    }catch(IOException ex){
+        System.out.println(ex.getMessage());
+    }
+    
+    }
+    
     public static void main(String [] array)throws IOException{
         Socket socket = new Socket("localhost",5000);
         System.out.println("Connected to server");
