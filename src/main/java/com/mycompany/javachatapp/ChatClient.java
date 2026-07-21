7caeabd31940b6048922643ce044c2a8b23f88e3
ASUS <ASUS@LAPTOP-LP5CEJ16>
2026-07-21 20:14:45 +0300

    conf chat clint add socket print writer , buffer reader , to make the clints ready to connect to the server in the constructor of ClatClient class

--- src/main/java/com/mycompany/javachatapp/ChatClient.java
+++ src/main/java/com/mycompany/javachatapp/ChatClient.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.javachatapp;
+
 import java.io.*;
 import java.net.*;
 
@@ -11,29 +12,39 @@ import java.net.*;
  * @author ammar
  */
 public class ChatClient {
-   
-    private Socket socket=null;
-    private BufferedReader inputConsole=null;
-    private PrintWriter out = null;
-    private BufferedReader in= null;
 
-    public ChatClient(String address, int port) {
-    try {
-        socket= new Socket(address,port);
-        System.out.println("Connetecd to the chat server");
-        inputConsole = new BufferedReader(new InputStreamReader(System.in));
-        out = new PrintWriter(socket.getOutputStream(),true);
-        in= new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        
-    }catch(IOException ex){
-        System.out.println(ex.getMessage());
+    private Socket socket = null;
+    private BufferedReader inputConsole = null;
+    private PrintWriter out = null;
+    private BufferedReader in = null;
+
+    public ChatClient(String address, int port) throws IOException {
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connetecd to the chat server");
+            inputConsole = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String line = "";
+            while (!line.equals("exit")) {
+                line = inputConsole.readLine();
+                out.println(line);
+                System.out.println(in.readLine());
+                socket.close();
+                inputConsole.close();
+
+            }
+        } catch (UnknownHostException u) {
+            {
+                System.out.println("host error exception" + u.getMessage());
+            }
+        } catch (IOException i) {
+            System.out.println("Unexpected exception: " + i.getMessage());
+        }
     }
-    
+
+    public static void main(String args[]) throws IOException {
+        ChatClient client = new ChatClient("127.0.0.1", 5000);
     }
-    
-    public static void main(String [] array)throws IOException{
-        Socket socket = new Socket("localhost",5000);
-        System.out.println("Connected to server");
-    }
-    
+
 }
